a8f304652e7bb637e362d5e8410c0a5bc5f5003f
nowdaysmen-sudo <nowdaysmen@gmail.com>
2026-02-04 18:51:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from flask import Flask
-import threading
-import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "OK"
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت شغال")
+    await update.message.reply_text("البوت شغال ✅")
 
-def run_bot():
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
-
-def run_server():
-    app.run(host="0.0.0.0", port=8000)
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    run_server()
+    main()
