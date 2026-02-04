85ec87c452a5776fc95a8bf9d7b73d7f33c77ada
nowdaysmen-sudo <nowdaysmen@gmail.com>
2026-02-04 17:43:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,33 @@
 import os
-from flask import Flask
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import threading
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import CommandHandler, Application
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+bot = Bot(token=BOT_TOKEN)
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Bot is running"
+application = Application.builder().bot(bot).build()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context):
     await update.message.reply_text("البوت شغال ✅")
 
-def run_bot():
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+application.add_handler(CommandHandler("start", start))
 
-def run_server():
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+
+@app.route("/", methods=["POST"])
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    await application.process_update(update)
+    return "ok"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Bot is running"
+
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    run_server()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
